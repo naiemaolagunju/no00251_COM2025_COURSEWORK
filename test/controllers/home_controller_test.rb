069302033dac10b969dcd4bb3ada752207075207
naiema <no00251@surrey.ac.uk>
2019-12-11 02:05:50 +0000

    Added testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,14 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
     assert_response :success
+
+    assert_select 'title', 'MusicLibrary'
+    assert_select 'h1', 'Music Library'
+    assert_select 'p', 'Welcome to MusicLibrary, A web app which can be used to store and organise all your favourite tunes.'
   end
+
+
 
 end
